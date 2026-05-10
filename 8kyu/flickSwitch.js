4b9d8487f0e96259436ac39e77232115f87169e2
orlovaDev <anastasiya.orlova1305@gmail.com>
2026-05-10 21:45:56 +0300

    solution FlickSwitch

---
+++ 8kyu/flickSwitch.js
@@ -0,0 +1,37 @@
+// ❓УСЛОВИЕ:
+
+//Создайте функцию, которая всегда возвращает True/True для каждого элемента в заданном списке.
+// Однако, если элемент — это слово «flick», переключитесь на возврат противоположного логического значения.
+//
+// Примеры:
+// ['codewars', 'flick', 'code', 'wars'] ➞ [True, False, False, False]
+//
+// ['flick', 'chocolate', 'adventure', 'sunshine'] ➞ [False, False, False, False]
+//
+// ['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
+
+// Примечания:
+// "flick" всегда будет отображаться в нижнем регистре.
+// Список может содержать несколько элементов типа flick.
+// Меняйте логическое значение для того же элемента, что и сам flick.
+
+// ✅ РЕШЕНИЕ1:
+
+function flickSwitch(arr) {
+  let state = true;
+  return arr.map(el => el === 'flick' ? state = !state : state);
+}
+
+
+// ✅ РЕШЕНИЕ2:
+
+
+function flickSwitch(arr) {
+  let state = true
+  return arr.map(el => {
+    if (el === 'flick') {
+      state = !state
+    }
+    return state
+  })
+}
