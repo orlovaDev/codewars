5c29b1b3fe474f0b0da1c43f19832b4f49e07c9f
orlovaDev <anastasiya.orlova1305@gmail.com>
2026-06-06 01:15:34 +0300

    solutionOppositesAttract2

--- 8kyu/oppositesAttract.js
+++ 8kyu/oppositesAttract.js
@@ -6,10 +6,14 @@
 
 // ✅ РЕШЕНИЕ
 
-function lovefunc(flower1, flower2) {
-  if (flower1 % 2 === 0 && flower2 % 2) return true;
-  if (flower1 % 2 && flower2 % 2 === 0) return true;
-    return false;
+// function lovefunc(flower1, flower2) {
+//   if (flower1 % 2 === 0 && flower2 % 2) return true;
+//   if (flower1 % 2 && flower2 % 2 === 0) return true;
+//     return false;
+// }
+
+function lovefunc(flower1, flower2){
+  return (flower1 % 2 === 0 && flower2 % 2 !== 0) || (flower1 % 2 !== 0 && flower2 % 2 === 0);
 }
 
 
