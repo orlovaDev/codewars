f964c39cce8392d91514a63776aa806b5e3ad3cc
orlovaDev <anastasiya.orlova1305@gmail.com>
2026-09-03 22:41:42 +0300

    grasshopperGradeBookSolution2

--- 8kyu/grasshopperGradeBook.js
+++ 8kyu/grasshopperGradeBook.js
@@ -11,19 +11,25 @@
 
 // Все проверяемые значения находятся в диапазоне от 0 до 100. Нет необходимости проверять отрицательные значения или значения больше 100.
 
-// ✅ РЕШЕНИЕ:
+// ✅ РЕШЕНИЕ 1:
+//
+// function getGrade(s1, s2, s3) {
+// let average = (s1 + s2 + s3) / 3
+//   if (average >= 90 && average <= 100) {
+//     return 'A'
+//   } else if (average >= 80 && average <= 90) {
+//     return 'B'
+//   } else if (average >= 70 && average <= 80) {
+//     return 'C'
+//   } else if (average >= 60 && average <= 70) {
+//     return 'D'
+//   } else if (average >= 0 && average <= 60) {
+//     return 'F'
+//   }
+// }
 
-function getGrade(s1, s2, s3) {
-let average = (s1 + s2 + s3) / 3
-  if (average >= 90 && average <= 100) {
-    return 'A'
-  } else if (average >= 80 && average <= 90) {
-    return 'B'
-  } else if (average >= 70 && average <= 80) {
-    return 'C'
-  } else if (average >= 60 && average <= 70) {
-    return 'D'
-  } else if (average >= 0 && average <= 60) {
-    return 'F'
-  }
-}
+// ✅ РЕШЕНИЕ 2:
+
+const getGrade = (s1, s2, s3, average = (s1 + s2 + s3) / 3) =>
+  average >= 90 ? "A" : average >= 80 ? "B" : average >= 70 ? "C" : average >= 60 ? "D" : "E";
+
